0b84f256240af1ecf8724655ae213cf61a30f50b
LP <grofit1@yahoo.com>
2025-06-04 14:34:05 +0100

    Adding github action

--- build/pack.bat
+++ build/pack.bat
@@ -1,2 +1,2 @@
-set version=0.2.0
-dotnet pack ../src/EcsRx.MonoGame -c Release -o ../../_dist /p:version=%version%
+set version=0.5.0
+dotnet pack ../src/EcsR3.MonoGame -c Release -o ../../_dist /p:version=%version%
